55c5d3d44060978128cf0fdb14fee55434cec08e
Kiwouitcg <165523154+Kiwouitcg@users.noreply.github.com>
2024-09-03 19:50:39 +0200

    Update chapitre 1.py

--- Exo piscine/chapitre 1.py
+++ Exo piscine/chapitre 1.py
@@ -166,6 +166,78 @@ def exercice_18():
         print(f"les  position de a sont :{position}")
         position = s.find('a', position + 1)
     
+def exercice_19():
+    #Ecrire un programme en Python qui permet de lister les chaines qui composent la liste **L = ["laptop", "iphone", "tablet"]** tout en indiquant la longueur de chaque chaine.
+    L = ["laptop", "iphone", "tablet"]
+    #lister les chaines
+    #Longeur de chaque chaine
+    
+    longeur = []
+    for i in L:
+        longeur = len(i)
+        print(f" le nom {i} fait {longeur} de longeurs")
+    
+def exercice_20():
+    #Ecrire un programme en langage Python, permettant d’échanger le premier et le dernier caractère d’une chaine donnée.## Exercice 20 - `input()`, `indexation`, `concaténation`, `print()`
+
+    C = input("ecris un truc : ")
+    premier = C[0] 
+    dernier = C[-1] 
+    corp = C[1:-1] 
+    resultat = dernier + corp + premier 
+    print(resultat)
+    
+def exercice_21():
+    #Ecrire un programme en langage Python, qui permet de compter le nombre de voyelles dans une chaine donnée. Exemple pour la chaine **s='anticonstitutionellement'** le programme doit renvoyer le message suivant : *"La chaine anticonstitutionellement possède 10 voyelles"*.
+    #definir les voyelles
+    #compter dans s 
+    s = "anticonstitutionellement"
+    voyelles = ['a', 'e', 'i', 'o', 'u', 'y']
+    nb = 0
+    for i in s:
+        if i in voyelles:
+            nb += 1
+    print(f"La chaine anticonstitutionellement possède {nb} voyelles")
+    
+def exercice_22():
+    #Ecrire un programme en Python, qui permet de renvoyer le premier mot d’un texte donné. Exemple pour le texte : **t = 'Python est un merveilleux langage de programmation'**, le programme doit renvoyer *"Python"*.   
+    t = input("ecris une phrase composé de plusieurs mots : ")
+    liste = t.split()
+    
+    print(liste[0])
+
+def exercice_23():
+    #Ecrire un programme en langage Python qui demande à l’utilisateur de saisir le nom d’un fichier et de lui renvoyer son extension. Exemple si l’utilisateur saisie **"coursPython.pdf"** le programme lui renvoie le message *"L’extension du fichier est .pdf"*.
+    s = input("ecris le nom du fichier.extension: ")
+    
+    split = s.split('.')
+    extension = split[-1]
+    
+    print(f" L extension du fichier est . {extension} ")
+
+def exercice_24():
+    #Un palindrome est un mot dont l'ordre des lettres reste le même si on le lit de gauche à droite ou de droite à gauche. Par exemple : **'laval' , 'radar', 'sos'**... sont des palindromes. Ecrire un programme en Python qui demande à l'utilisateur de saisir un mot et de lui renvoyer s'il s'agit d'un palindrome ou non.
+    s = input("ecrit un mot : ")
+    inverse = s[::-1]
+    if s == inverse:
+        print("C'est un palindrome")
+    else: print("non")
+
+def exercice_25():
+    #Ecrire un programme qui demande à l’utilisateur de saisir un mot et de lui renvoyer son inverse. Exemple si l’utilisateur saisi le mot **"python"**, le programme lui renvoie *"nohtyp"*.
+    s = input('ecrit un mot :')
+    inverse = s[::-1]
+    
+    print(inverse)
+
+def exercice_26():
+    ### Exercice 26 Ecrire un programme qui demande à l’utilisateur de saisir un texte et de lui renvoyer tous les mots commençant par la lettre **a**.
+    s = input("ecrit un texte : ")
+    split = s.split()
+    for i in split:
+        if i.startswith("a"):
+            print(i)
+
 
 
 
